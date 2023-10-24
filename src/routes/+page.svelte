3f36506e54ec33fabfe8c10d63dacbedb6cb1314
SkuliX01 <skulix137@proton.me>
2023-10-24 13:45:10 +0200

    Revert to Version 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     <div class="flex justify-center mt-[2rem]">
         <div class="w-[320px] h-[635px] border-[5px] border-[#c9c9c9] rounded-[45px] pr-4">
             <div class="w-[310px] h-[625px] border-[8px] border-black rounded-[40px] absolute justify-center items-center">
-                <div class="w-[110px] h-[27px] absolute mt-[9px] ml-[6rem] rounded-full bg-black static animate-scaleOutIs active:animate-scaleIs">
+                <div class="w-[110px] h-[27px] absolute mt-[9px] ml-[6rem] rounded-full bg-black static animate-scaleOutIs hover:animate-scaleIs">
                     <div class="w-[15px] h-[15px] bg-[#3d3d3d] mt-[6px] rounded-full ml-[85px] flex ">
                         <div class="w-[5px] h-[5px] bg-black mt-[5px] rounded-full ml-[5px]">
                         </div>
